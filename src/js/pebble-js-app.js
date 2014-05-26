a78991c3c93e3b83415c46c53e8d08024f46e5a7
Andy Clark <ajc97@cantab.net>
2014-05-26 23:17:10 +0100

    JavaScript experiments

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -3,3 +3,8 @@ Pebble.addEventListener("ready",
         console.log("Hello world! - Sent from your javascript application.");
     }
 );
+Pebble.addEventListener("showConfiguration", function() {
+		console.log("showConfiguration event");
+		Pebble.openURL("http://www.realvnc.com");
+	}
+);
